d744168d189333ca89a365e378a4d1b4909ea063
aravindbalakumar <101953156+aravindbalakumar@users.noreply.github.com>
2024-12-03 20:20:46 +0000

    Initial projects fixes
    Initial projects fixes

--- src/Pages/Projects/Project.js
+++ src/Pages/Projects/Project.js
@@ -1,6 +1,6 @@
 import "./project.css"
 import imgTemp from "./../../BlogAssets/Images/0.jpg";
-import { TbBrandUnity } from "react-icons/tb";
+import { TbBrandUnity ,TbBrandGithub} from "react-icons/tb";
 
 function Project({title="", miniDesc="", skill=""})
 {
@@ -10,8 +10,9 @@ function Project({title="", miniDesc="", skill=""})
             <div id="prjt_card_title">{title}</div>
             <div id="prjt_card_image_container"><img id="prjt_card_image" src={imgTemp}></img></div>
             <div id="prjt_card_miniDescription">{miniDesc}</div>
-            {/* <div id="prjt_card_skills">{skill}</div> */}
-            <div id="prjt_card_skills"><TbBrandUnity /></div>
+            <div id="prjt_card_skills"><TbBrandUnity id="prjt_card_skill_Tag"/>
+            <TbBrandGithub id="prjt_card_skill_Tag"/>
+            </div>
             <div id="prjt_card_viewMore">
                 <button id="button">VIEW MORE</button>
             </div>
